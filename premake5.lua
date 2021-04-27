f494fad97cc4169dd6822bbe1ea8cc7fcd6cb83d
Athang <xdevapps>
2021-04-27 16:33:24 +0530

    Init for ApexGameEngine submodule

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+project "RTTR"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/rttr/**.cpp",
+		"src/rttr/**.h"
+	}
+
+	includedirs {
+		"src/"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
